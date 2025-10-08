0237dc2d69529656254b196de16b25d2fae2fed8
Alamin4D <alaminahmed79000@gmail.com>
2025-10-08 12:32:27 +0600

    added the newsletterSection

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import ServicesSection from './components/ServicesSection'
 import TestimonialsSection from './components/TestimonialsSection'
 
 function App() {
-  
   return (
     <main className="relative min-h-screen overflow-x-hidden">
       <div className="absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10"></div>
